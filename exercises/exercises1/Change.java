20ce3a11ad56dc3e9e237a77e21b762b0a68e0e5
linhongjin <linhongjin@DESKTOP-MHR11P7>
2017-01-17 20:11:37 -0600

    commit

--- exercises/exercises1/Change.java
+++ exercises/exercises1/Change.java
@@ -8,8 +8,23 @@ public class Change {
 		//
 		// Below, prompt the user to enter a number of pennies
 		//
-		
-		
+	ArgsProcessor ap=new ArgsProcessor(args);
+	int InitialPennies=ap.nextInt("How many pennies?");
+	int d=InitialPennies/100;
+	int PenniesLeftAfterDollars=InitialPennies%100;
+	int q=PenniesLeftAfterDollars/25;
+	int PenniesLeftAfterQuarters=PenniesLeftAfterDollars%25;
+	int dimes=PenniesLeftAfterQuarters/10;
+	int PenniesLeftAfterDimes=PenniesLeftAfterQuarters%10;
+	int n=PenniesLeftAfterDimes/5;
+	int Pennies=PenniesLeftAfterDimes%5;
+    
+	
+	System.out.println(d+" dollars");
+	System.out.println(q+" quarters");
+	System.out.println(dimes+" dimes");
+	System.out.println(n+" nickles");
+	System.out.println(Pennies+" Pennies");
 		//
 		// Then, compute and print out how many 
 		//    dollars, quarters, dimes, nickels, and pennies
