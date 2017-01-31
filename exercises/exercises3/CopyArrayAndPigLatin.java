07e7528f9dbcf091496d9455a7e86a85907e4f5b
Administrator <Administrator@DESKTOP-SCP6P56>
2017-01-30 23:33:52 -0600

    exercise 3

--- exercises/exercises3/CopyArrayAndPigLatin.java
+++ exercises/exercises3/CopyArrayAndPigLatin.java
@@ -10,15 +10,23 @@ public class CopyArrayAndPigLatin {
 		//  So we cannot just do the following (too much to type):
 		//     String[] copy = { names[0], names[1], names[2] };
 		//
-		
-		
+		String [] copy=new String[names.length];
+		for(int i=0;i<names.length;i++){
+			copy[i]=names[i]+"ay";
+		}
+		for(int i=0;i<names.length;i++){
+			System.out.println(names[i]);
+			System.out.println(copy[i]);
+		}
 		// 
 		// Follow the instructions on the web page to check
 		// that your copy is correct.  Then cause the copy to store the
 		// copied strings in "Pig Latin".  Do this by iteration without
 		// assuming the length or contents of the names array.
 		//
-		
+		for(String s:copy){
+			System.out.println("the copy is "+s);
+		}
 	}
 
 }
--- exercises/exercises3/Shuffle.java
+++ exercises/exercises3/Shuffle.java
@@ -11,7 +11,15 @@ public class Shuffle {
 		for (int i=0; i < original.length; ++i) {
 			System.out.println("Original at " + i + " is " + original[i]);
 		}
-
+        String[] shuffles=new String[original.length];
+        for(int i=shuffles.length-1;i>=0;i--){
+        	int c=(int)(Math.random()*(i+1));
+        	shuffles[i]=original[c];
+        	for(int j=c;j<original.length-1;j++){
+        		original[j]=original[j+1];
+        	}}
+        
+        System.out.println("");
 		//
 		// Follow the instructions on the web page to make a copy of
 		// the original array, named shuffled, but with its elements
@@ -19,7 +27,8 @@ public class Shuffle {
 		// shuffled array contains the same strings, but in a randomized
 		// order.
 		//
-
-	}
+        for (int k=0; k < shuffles.length; ++k) {
+			System.out.println("Shuffled at " + k + " is " + shuffles[k]);
+		}
+	}}
 	
-}
