6e3a401fc28752abbfa0bdad4b26aa995f240a82
Administrator <Administrator@DESKTOP-SCP6P56>
2017-02-16 22:25:18 -0600

    lab4 finish

--- labs/lab4/BumpingBalls.java
+++ labs/lab4/BumpingBalls.java
@@ -2,15 +2,19 @@ package lab4;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 import cse131.ArgsProcessor;
 import sedgewick.StdAudio;
 import sedgewick.StdDraw;
 import sedgewick.StdIn;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
 
 public class BumpingBalls {
 	
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		ArgsProcessor ap = new ArgsProcessor(args);
     	int ball = ap.nextInt("How many balls?");
     	int t=ap.nextInt("How many iterations?");
@@ -28,33 +32,41 @@ public class BumpingBalls {
         }
         double radius = 0.05;
         int k=0;
+        for(int i=0;i<=ball-2;i++){
+    		for(int j=i+1;j<=ball-1;j++){
+    			while(Math.pow((rx[i]-rx[j]),2)+Math.pow((ry[i]-ry[j]), 2)<=4*Math.pow(radius, 2)){
+    			rx[i]=Math.random()*1.9-0.95;
+    			rx[j]=Math.random()*1.9-0.95;
+    			ry[i]=Math.random()*1.9-0.95;
+    			ry[j]=Math.random()*1.9-0.95;
+    			
+    			}
+    		}
+    		
+    	}
+        
         while(k<=t){
-        	for(int i=0;i<=ball-1;i++){
-        	if (Math.abs(rx[i] + vx[i]) > 1.0 - radius) {vx[i] = -vx[i];}
-        	if (Math.abs(ry[i] + vy[i]) > 1.0 - radius) {vy[i] = -vy[i];}}
+        	
         	
    
         	for(int i=0;i<=ball-2;i++){
         		for(int j=i+1;j<=ball-1;j++){
-        			if(Math.pow((rx[i]-rx[j]+vx[i]-vx[j]),2)+Math.pow((ry[i]-ry[j]+vx[i]-vy[j]), 2)<=4*Math.pow(radius, 2)){
+        			 if(((Math.pow((rx[i]-rx[j]+vx[i]-vx[j]),2)+Math.pow((ry[i]-ry[j]+vy[i]-vy[j]), 2)<=4*Math.pow(radius, 2)))&&(Math.pow((rx[i]-rx[j]),2)+Math.pow((ry[i]-ry[j]), 2)>=4*Math.pow(radius, 2))){
         				vx[i]=-vx[i];
         				vx[j]=-vx[j];
         				vy[i]=-vy[i];
         				vy[j]=-vy[j];
+        			    StdAudio.play("sound/boing.au");
         			
         			}
-        		}
-        		
-        	}
+        		}}
+        	for(int i=0;i<=ball-1;i++){
+            	if (Math.abs(rx[i] + vx[i]) > 1.0 - radius) {vx[i] = -vx[i];}
+            	if (Math.abs(ry[i] + vy[i]) > 1.0 - radius) {vy[i] = -vy[i];}}
         	for(int a=0;a<=ball-1;a++){
         		rx[a] = rx[a] + vx[a]; 
                 ry[a] = ry[a] + vy[a];
-                if(rx[a]>0.95){
-                	rx[a]=0.95;
-                }
-                if(ry[a]>0.95){
-                	ry[a]=0.95;
-                }
+                
         	}
         	StdDraw.setPenColor(StdDraw.GRAY);
             StdDraw.filledSquare(0, 0, 1.0);
