9f8307c2c5715903b06ab06a3470c1ce87ff979f
Administrator <Administrator@DESKTOP-SCP6P56>
2017-02-20 22:33:50 -0600

    exercise 5

--- exercises/exercises5/StringMethods.java
+++ exercises/exercises5/StringMethods.java
@@ -12,16 +12,39 @@ public class StringMethods {
 	// First one is completed for you:
 	//
 	
-	public static String selfConcat(String s) {
-		return s + s;
+	public static String dup(String s) {
+		return copies(s,2);
 	}
-	
+	public static String copies(String s, int n){
+		String array[]=new String[n];
+		for(int i=0;i<n;i++){
+			array[i]=s;
+		}
+		return join(array,"");
+	}
+	public static String join(String array[],String joiner){
+		if(array.length==0){return "";}
+		String ans=array[0];
+		for(int i=1;i<array.length;i++){
+			ans=ans+joiner+array[i];
+		}
+		return ans;
+	}
 	public static void main(String[] args) {
 		
 		//
 		// Testing the methods
 		//
-		System.out.println(selfConcat("echo"));
+		System.out.println(dup("echo"));
+		for(int i=0;i<5;i++){
+			System.out.println(copies("CS501 ",i));
+		}
+		String s="I love CS";
+		String []words=s.split(" ");
+		for(String w:words){
+			System.out.println("A word: "+w);
+		}
+		System.out.println("put together: "+join(words," "));
 	}
 
 }
