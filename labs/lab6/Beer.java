56fda95f526c1a07ba59faefc7896e78dab5dea4
Administrator <Administrator@DESKTOP-SCP6P56>
2017-03-07 10:39:23 -0600

    lab6 revised

--- labs/lab6/Beer.java
+++ labs/lab6/Beer.java
@@ -3,10 +3,11 @@ package lab6;
 public class Beer {
 	public static String bottlesOfBeer(int n){
 		String s="";
-		while (n>0){
-		s=s+"\n"+n+" bottles of beer on the wall, "+n+" bottles of beer; you take one down, pass it around, "+(n-1)+" bottles of beer on the wall.";
-		n=n-1;}
+		s="\n"+n+" bottles of beer on the wall, "+n+" bottles of beer; you take one down, pass it around, "+(n-1)+" bottles of beer on the wall.";
+		if(n>1){
+		s=s+bottlesOfBeer(n-1);}
 		return s;
+		
 		
 	}
 	public static void main(String[] args) {
