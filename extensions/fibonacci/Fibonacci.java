ef86c24451e90b453a90ce1404d663497915f1f6
Administrator <Administrator@DESKTOP-SCP6P56>
2017-03-10 13:39:30 -0600

    extension 6.4

--- extensions/fibonacci/Fibonacci.java
+++ extensions/fibonacci/Fibonacci.java
@@ -8,7 +8,13 @@ public class Fibonacci {
 	 * @return fib(n), computed recursively
 	 */
 	public static int recursive(int n) {
-		return 0; // FIXME
+		if(n<=1){
+			 return n;
+		 }
+		 else{
+			 return recursive(n-1)+recursive(n-2)
+;
+		 } // FIXME
 	}
 	
 	/**
@@ -17,7 +23,17 @@ public class Fibonacci {
 	 * @return fib(n), computed iteratively
 	 */
 	public static int iterative(int n) {
-		return 0; // FIXME
+		if(n==0){return 0;}
+		else if(n==1){return 1;}
+		else{
+		int a[]=new int [n+1];
+		a[0]=0;
+		a[1]=1;
+		for(int i=2;i<=n;i++){
+			a[i]=a[i-1]+a[i-2];
+		}
+		return a[n];}
+		// FIXME
 	}
 
 }
