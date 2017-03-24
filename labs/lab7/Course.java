d389590edd3fc7f57053925b654cf53327635d99
Administrator <Administrator@DESKTOP-SCP6P56>
2017-03-23 23:57:58 -0500

    lab 7

--- labs/lab7/Course.java
+++ labs/lab7/Course.java
@@ -4,7 +4,9 @@ public class Course {
    private String name;
    private int credits;
    private int seats;
-   private String roster[];
+   private String roster;
+   private int studentin;
+   private double gpasum;
 public Course(String name, int credits, int seats) {
 	this.name = name;
 	this.credits = credits;
@@ -20,16 +22,30 @@ public int getRemainingSeats() {
 	return seats;
 }
 
-
-public String[] getRoster() {
+public int getStudentin() {
+	return studentin;
+}
+public String getRoster() {
 	return roster;
 }
 public boolean addStudent(Student a){
-	for(int i=0;i<roster.length;i++){
-		if(a.getName()==roster[i]){return false;}
+	if(seats>0){
+		this.roster=this.getRoster()+" "+a.getName();
+		seats=seats-1;
+		studentin=studentin+1;
+		gpasum=gpasum+a.getGPA();
+		return true;
 	}
-	seats=seats-1;
-	return true;
+	else{
+		return false;
+	}
 }
+public double averageGPA(){
+	double average=gpasum/studentin;
+	return average;
+}
+
+public String toString() {
+		return name+credits+seats;}
    
 }
